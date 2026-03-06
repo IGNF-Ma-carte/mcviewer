18440a456669ec3522a35e7e0019a2fb0737c6ef
viglino <jean-marc.viglino@ign.fr>
2026-03-06 11:42:10 +0100

    [VER] 4.6.2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import setPiwik from 'mcutils/charte/setPiwik'
 import story from './storymap'
 // Add slide for video
 import 'mcutils/charte/mcPrez'
+
+// Font Open sans
+import "@fontsource/open-sans/800.css";
+import "@fontsource/open-sans/800-italic.css"; 
+import "@fontsource/open-sans/700.css";
+import "@fontsource/open-sans/700-italic.css"; 
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/600-italic.css"; 
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/500-italic.css"; 
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/400-italic.css"; 
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/300-italic.css"; 
+// Fonts Fira sans
+import '@fontsource/fira-sans/800.css'
+import '@fontsource/fira-sans/800-italic.css'
+import '@fontsource/fira-sans/700.css'
+import '@fontsource/fira-sans/700-italic.css'
+import '@fontsource/fira-sans/600.css'
+import '@fontsource/fira-sans/600-italic.css'
+import '@fontsource/fira-sans/500.css'
+import '@fontsource/fira-sans/500-italic.css'
+import '@fontsource/fira-sans/400.css'
+import '@fontsource/fira-sans/400-italic.css'
+import '@fontsource/fira-sans/300.css'
+import '@fontsource/fira-sans/300-italic.css'
+
 // API
 import api from './api/api'
 import './api/center'
