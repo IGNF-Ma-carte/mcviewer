19c9888cf5a9962d0f41dbf29426c137f6af5eae
viglino <jean-marc.viglino@ign.fr>
2026-03-27 12:51:04 +0100

    [BUILD]

--- src/storymap.js
+++ src/storymap.js
@@ -81,7 +81,6 @@ story.on('read', () => {
 
         // Reload layers
         map.getLayers().forEach(l => {
-          if (l.get('type') === 'file') l.set('reload', 6);
           if (l.getSource() && l.get('reload') && parseFloat(l.get('reload'))) {
             const time = parseFloat(l.get('reload')) * 1000;
             function reload() {
